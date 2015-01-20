368d377539135e9ffa61e3233e97ef7b9d454fb7
rahul patil <rahul.deshmukhpatil@gmail.com>
2015-01-20 09:32:23 +0530

    [LPHW]: chapter 35-38: while loop, commands and lists

---
+++ LearnPythonHardWay/LPHW-35-38.py
@@ -0,0 +1,21 @@
+"""
+	Program for while loop and lists 
+"""
+
+listA = ['ram', 'sham', 'gyan']
+
+listB = ['seeta', 'geeta', 'neeta', 'seema']
+
+while (len(listA) < 6) and (len(listB) > 0): 
+	lastElementOfB = listB.pop()
+	print "removing %s from listB and adding to listA" %(lastElementOfB)
+	listA.append(lastElementOfB);
+
+print "List A is now: %s" %(listA)
+print "List B is now: %s" %(listB)
+
+print "ListA with intermittent #:%s" %('#'.join(listA))
+print "first element of A:%s" %(listA[0])
+print "last element of A:%s" %(listA[-1])
+
+
