99977954a8f3dfde84a917c847b9f129e9adaf49
rahul patil <rahul.deshmukhpatil@gmail.com>
2015-01-21 09:15:31 +0530

    [LPHW-39]: dict usage

---
+++ LearnPythonHardWay/LPHW-39.py
@@ -0,0 +1,30 @@
+"""
+	LPHW-39
+	Dicts
+"""
+
+#define a dict
+states={'Maharashtra':'MH', 'Karnataka':'KA', 'Madhya Pradesh':'MP'}
+
+#print all states are
+def printStates(states):
+	print "States are"
+	i = 0
+	for state,abbr in  states.items():
+		print "%d> %s:%s" %(i, state, abbr)
+		i+=1
+	print
+
+printStates(states)
+
+states['Andhra Pradesh']='AP'
+
+printStates(states)
+
+abbr = states.get('Karnataka', None)
+print "Abbr for Karnataka : %s" %(abbr)
+
+if(abbr == None):
+	print "No Abbr for Karnataka"
+
+
