526cf69468a5f76f8d05f7764ec12b85b0452b46
Rahul Patil <rahul.deshmukhpatil@gmail.com>
2015-01-22 12:26:02 +0530

    [LPHW-1-5]: added comments

--- LearnPythonHardWay/LPHW-1-5.py
+++ LearnPythonHardWay/LPHW-1-5.py
@@ -1,9 +1,20 @@
 # program for chapt 1 to 5
 
+#sinmple print
 print "Variable manipulation"
+
+#initialize the variable a. 
+# a has type of whatever on right side of =.
+# no need of declaring type explicitly
 a = 5
+
+#delcare B
 b = 6
+
+#initalize c with result of addition of a and b
 c = a + b 
+
+# print format is  print <string with format identifier> <variables inside %()saperated with ',' >
 print "Addition %d + %d = %d " %(a, b, c), "!!! IS not that great"
 
 
