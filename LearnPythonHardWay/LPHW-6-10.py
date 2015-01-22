1b64b8775c7e230681a160c7c8f4f55cfd2019ab
Rahul Patil <rahul.deshmukhpatil@gmail.com>
2015-01-22 12:28:07 +0530

    [LPHW-6-10]: added comments

--- LearnPythonHardWay/LPHW-6-10.py
+++ LearnPythonHardWay/LPHW-6-10.py
@@ -4,13 +4,17 @@ print "Print manipulation"
 print "This does not appear",
 print "on new line. see code !!!"
 print "Hey * 10 = ", "Hey %s " %("!!") * 10
+
+#multiline print
 print """
 		This is a paragraph
 		Put as many lines as you can here
 """
 
+
 print "Going to test while loop"
-#while True:
+# For loop with the vector as input.
+# i is set to value of each element of vector/list one by one
 for i in ["Ramesh", "Suresh", "Mahesh", "Ganesh"]:
 	print "%r \a !!!" %(i)
 
