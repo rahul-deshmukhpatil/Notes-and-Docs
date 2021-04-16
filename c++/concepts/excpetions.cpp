ee1672022c989b1fb75a007dc7bbf08128ca69f5
rahul patil <rahul.deshmukhpatil@gmail.com>
2021-04-16 10:31:15 +0100

    added the c++ templates documentation

---
+++ c++/concepts/excpetions.cpp
@@ -0,0 +1,4 @@
+CppCon 2019: Ben Saks "Back to Basics: Exception Handling and Exception Safety"
+https://www.youtube.com/watch?v=W6jZKibuJpU
+
+Exceptions are to avoid cascading error check routines
---
+++ python/documentation/python_tutorial.py
@@ -0,0 +1,19 @@
+3. An Informal Introduction to Python
+    3.1.1. Numbers
+        ** has higher precedence over -, so -3**2 = -(3**2)
+        Decimal
+        Fraction
+            as_integer_ratio()
+            limit_denominator()
+
+    3.1.2 string
+        print('\
+            firt line') : \ at the end of line will ignore new line
+        3 * str => 3 time string str
+        'print double quote => " '
+        "print single quote => ' "
+        slicing works without any index error
+
+    3.1.3 list : compound data type
+        copy.copy() : shallow copy
+        copy.deepcopy() : deep copy
