ddf18b1584d0ea19c7ddc3ac2cb3b3fb10e75e7c
rahul patil <rahul.deshmukhpatil@gmail.com>
2024-11-07 08:15:45 +0000

    default commit message

--- c++/cppref/containers/vector.cpp
+++ c++/cppref/containers/vector.cpp
@@ -1,1 +1,28 @@
-contigious memory
+contigious mem.
+automatic expansion & always extra mem allocated than needed.
+
+reserve(): because allocat are costly
+
+constexpr vectors example.
+
+elements must be erasable requirement
+
+Incomplete type T, vector<T> if allowed if allocator is complete type
+
+difference-type ptrdiff_t, can have negative value
+
+reverse iterator begin == std::prev(frontit).
+
+assign 
+assign_range
+
+at -> returns ref and checks bounds
+
+erase
+pop_back
+
+emplace_back
+append_range
+swap: swaps the memory data ptr, iterators remain valid pointing to original vector
+
+three way operator.
