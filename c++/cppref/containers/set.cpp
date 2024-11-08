cf9cb5de97644133298cd1a2fc081eee3f650ed7
rahul patil <rahul.deshmukhpatil@gmail.com>
2024-11-08 10:57:01 +0000

    default commit message

---
+++ c++/cppref/containers/set.cpp
@@ -0,0 +1,16 @@
+Set
+	sorted container
+
+	set<T, Compare = std::less<T>, Allocator = std::allocator<T>>
+
+	insert_range
+
+	insert (with hint, range, begin/end)
+	emplace_hint
+	 
+	swap : optimized no move, copy, or swap operations on individual elements.
+
+	extract: (key, pos itr, ) returns node_handle
+
+	contains
+	  
