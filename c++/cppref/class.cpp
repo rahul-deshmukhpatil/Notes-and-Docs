5da1b85898af3d85e8a68a9a531d3d43b529d316
rahul patil <rahul.deshmukhpatil@gmail.com>
2025-01-09 08:28:22 -0500

    default commit message

---
+++ c++/cppref/class.cpp
@@ -0,0 +1,12 @@
+Class has
+	members and funcs
+		static and non-static
+	enclosed enum and class decls
+	using class and enum
+	template members variables, funcs
+
+	polymorphic classes with virtual functions
+		both pointer and ref :
+			can be used to call correct virtual funcs
+			also typeid(*ptToBase) : gives derived typeid
+			also typeid(refToBase) : gives derived typeid
