f1f14f79e8686f7655de2f63b0da7f662b40f6cb
rahul patil <rahul.deshmukhpatil@gmail.com>
2025-01-09 19:12:32 -0500

    default commit message

--- c++/cppref/class.cpp
+++ c++/cppref/class.cpp
@@ -10,3 +10,31 @@ Class has
 			can be used to call correct virtual funcs
 			also typeid(*ptToBase) : gives derived typeid
 			also typeid(refToBase) : gives derived typeid
+	
+	Trivial :  
+		all base and members are trivial
+		has atleast 1 trivial copyable:
+			eligible(declared and not deleted)
+			trivial: doesnt do anything special and byte by byte copying works
+			because compiler might optimize default trivial constr to faster
+			byte by byte copying
+
+			copy or move constr 
+			copy or move = op
+
+	standard layout: 
+		all base and members are standard layout
+		all members have only 1 access public/private/protected
+		only 1 class has non-static members
+		no virtual function
+
+		basically the compiler resolves to same binary format from class def
+		and objects could be consumed from both C or C++
+		could be read/written directly from file using de/serialization 
+
+
+
+			
+
+
+
