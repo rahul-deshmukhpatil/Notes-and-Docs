43ee2f7c4b9a2114e17f25bf05627a5d7afc64fe
rahul patil <rahul.deshmukhpatil@gmail.com>
2025-02-15 09:30:00 +0000

    default commit message

---
+++ c++/cppref/specs/c++20.cpp
@@ -0,0 +1,17 @@
+coroutines
+concepts
+three-way comparison
+designated initializers
+template syntax for lambdas
+range-based for loop with initializer
+[[likely]] and [[unlikely]] attributes
+deprecate implicit capture of this
+class types in non-type template parameters
+constexpr virtual functions
+explicit(bool)
+immediate functions
+using enum
+lambda capture of parameter pack
+char8_t
+constinit
+__VA_OPT__
