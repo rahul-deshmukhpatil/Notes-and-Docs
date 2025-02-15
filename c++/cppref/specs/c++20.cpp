a3450ebfc2b969041bc29ace9c42de5c5d607a06
rahul patil <rahul.deshmukhpatil@gmail.com>
2025-02-15 09:43:23 +0000

    default commit message

--- c++/cppref/specs/c++20.cpp
+++ c++/cppref/specs/c++20.cpp
@@ -1,6 +1,39 @@
 coroutines
 concepts
+
 three-way comparison
+
+provides three type of ordering
+	strong_ordering:  equal and equivalent
+	weak_ordering: only equivalent
+	partial_ordering: cant guarantee order
+
+	define the <=> operator with the ordering policy
+	use the space spaceship opeartor directly
+	if (auto cmp = p1 <=> p2; cmp == 0)
+
+	or and default the comparison operators == to use the <=>
+	for compiler generateor operator ==
+	p1 == p2
+	
+
+	struct P
+	{
+		int x{};
+		int y;
+
+		std::strong_ordering  operator<=>(const P& other) const
+		{
+			if (auto cmp = x<=> other.x; cmp!=0)
+				return cmp;
+			return y <=> other.y;
+		}
+
+		bool operator==(const P& other) const = default;
+	};
+
+
+
 designated initializers
 template syntax for lambdas
 range-based for loop with initializer
