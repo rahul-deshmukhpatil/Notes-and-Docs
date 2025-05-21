93cca6e0d4df5e5698cb4f92b53f38dadac5fbac
rahul patil <rahul.deshmukhpatil@gmail.com>
2025-05-21 14:29:42 +0100

    default commit message

--- c++/gtest.cpp
+++ c++/gtest.cpp
@@ -20,3 +20,7 @@ ASSERT_* stops the execution of test if test throws
 
 if you link againt lgtest_main it has definition for main function which runs
 the RUN_ALL_TESTS functions.
+
+you can pass different flags to control and run specific tests. 
+manually invoking InitGoogleTest() function reads all flags and runs the test
+but with lgtest-main you don't need to do this.
